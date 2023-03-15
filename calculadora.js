28cf836a8819c9066cf311ade09dae4cd60d454f
Diego Albuquerque <ds_albuquerque@hotmail.com>
2023-03-15 10:01:01 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -2,13 +2,11 @@
 const main = document.querySelector('main')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
+const root = document.querySelector(":root")
 
 input.focus()
 
-//criando um array com os caracteres que serão permitidos:
-const permitidos = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " ",]
-
-//funcionalidade de clicar nos botões:
+//funcionalidade para os botões:
 document.querySelectorAll('.charKey').forEach( (charKeyBtn) => {
   charKeyBtn.addEventListener('click', () => {
     const value = charKeyBtn.dataset.value
@@ -24,7 +22,37 @@ document.getElementById('clear').addEventListener('click', () => {
 
 document.getElementById('equal').addEventListener('click', calculate)
 
+function calculate(){
+  resultInput.value = 'ERRO - Revise o cálculo'
+  resultInput.classList.add('error')
+  input.focus()
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove('error')
+}
 
+// Alterando os temas:
+document.getElementById('themeSwitcher').addEventListener('click', () => {
+  console.log("ativou")
+  if (main.dataset.theme === 'dark'){
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = 'light'
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
+
+//criando um array com os caracteres que serão permitidos:
+const permitidos = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " ",]
+
+// funcionalidade de usar o teclado para realizar os calculos com as teclas permitidas:
 input.addEventListener('keydown', (e) => {
   e.preventDefault()
   
@@ -48,9 +76,15 @@ input.addEventListener('keydown', (e) => {
   }
 })
 
-
-function calculate(){
-  const result = eval(input.value)
-  resultInput.value = result
-  console.log(result)
-}
+//habilitando o botão de copiar o resultado:
+document.getElementById('copyToClipboard').addEventListener('click', (ev) => {
+  const button = ev.currentTarget
+  if (button.innerText === 'Copy') {
+    button.innerText = 'Copied!'
+    button.classList.add('success')
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = 'Copy'
+    button.classList.remove('success')
+  }
+})
