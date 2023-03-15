577744c2f5e02a12c6226317bd85b03722b70b99
Diego Albuquerque <ds_albuquerque@hotmail.com>
2023-03-15 00:22:31 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,17 +1,29 @@
-const main = document.querySelector('main')
 
+const main = document.querySelector('main')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
+
+input.focus()
 
 //criando um array com os caracteres que serão permitidos:
 const permitidos = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " ",]
 
+//funcionalidade de clicar nos botões:
 document.querySelectorAll('.charKey').forEach( (charKeyBtn) => {
   charKeyBtn.addEventListener('click', () => {
     const value = charKeyBtn.dataset.value
     input.value += value
   })
 })
+
+document.getElementById('clear').addEventListener('click', () => {
+  input.value = ''
+  resultInput.value = ''
+  input.focus()
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
 
 input.addEventListener('keydown', (e) => {
   e.preventDefault()
@@ -21,9 +33,14 @@ input.addEventListener('keydown', (e) => {
     return
   }
 
-  // Permite usar o botão backspace e quando o botão é pressionado é apagado o ultimo caractere da string
   if (e.key === 'Backspace') {
     input.value = input.value.slice(0,-1) // o slice deixa apenas do caractere 0 até o penultimo, assim eliminando o ultimo.
+  }
+
+  if (e.key === 'Escape') {
+    input.value = ''
+    resultInput.value = ''
+    return
   }
 
   if (e.key === 'Enter' || e.key === '=') {
@@ -33,7 +50,7 @@ input.addEventListener('keydown', (e) => {
 
 
 function calculate(){
-  console.log("calculado")
- 
-
+  const result = eval(input.value)
+  resultInput.value = result
+  console.log(result)
 }
